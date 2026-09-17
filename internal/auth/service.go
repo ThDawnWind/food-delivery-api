a76016688e90ceed3eb0bc8882f02aa33f2e3b70
Ayub Bashirov <135542028+ThDawnWind@users.noreply.github.com>
2026-09-17 15:24:50 +0300

    feat: add authentication service

---
+++ internal/auth/service.go
@@ -0,0 +1,59 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ThDawnWind/food-delivery-api/internal/user"
+)
+
+type UserAuthenticator interface {
+	Login(ctx context.Context, input *user.LoginUser) (*user.User, error)
+}
+
+type TokenGenerator interface {
+	Generate(userID int64, role string) (string, error)
+}
+
+type Service struct {
+	users  UserAuthenticator
+	tokens TokenGenerator
+}
+
+func NewService(users UserAuthenticator, tokens TokenGenerator) *Service {
+	return &Service{
+		users:  users,
+		tokens: tokens,
+	}
+}
+
+type LoginResult struct {
+	AccessToken string     `json:"access_token"`
+	User        *user.User `json:"user"`
+}
+
+func (s *Service) Login(ctx context.Context, input *user.LoginUser) (*LoginResult, error) {
+	userData, err := s.users.Login(
+		ctx,
+		input,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := s.tokens.Generate(
+		userData.ID,
+		string(userData.Role),
+	)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to generate access token: %w",
+			err,
+		)
+	}
+
+	return &LoginResult{
+		AccessToken: token,
+		User:        userData,
+	}, nil
+}
